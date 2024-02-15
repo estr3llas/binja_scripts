2809dd48377a9922ec83670991baaa29ba25db4a
estr3llas <126114608+estr3llas@users.noreply.github.com>
2024-02-15 01:50:55 -0300

    Update count_instr_per_sub.py

--- count_instr_per_sub.py
+++ count_instr_per_sub.py
@@ -1,4 +1,4 @@
-# This is script comes from the fact that most obfuscators tend to protect and obfuscate harder subroutines which are somehow important to the code.
+# This script comes from the idea that most obfuscators tend to protect and obfuscate harder subroutines which are somehow important to the code.
 # Obfuscators usually apply arithmetic and virtualization-based obfuscations, which increases a lot the size of the target subroutine.
 # Aware of that, listing the instructions per subroutine can lead you to key subroutines in the binary.
 
