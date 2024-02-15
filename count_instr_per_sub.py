acc037837ae0571b8b5c0057ebebb56d2fa014c0
estr3llas <126114608+estr3llas@users.noreply.github.com>
2024-02-15 01:39:53 -0300

    Update count_instr_per_sub.py

--- count_instr_per_sub.py
+++ count_instr_per_sub.py
@@ -1,5 +1,6 @@
 # This is script comes from the fact that most obfuscators tend to protect and obfuscate harder subroutines which are somehow important to the code.
-# Aware of that, the obfuscation comes in form of additional instructions, which, with this script, can lead you to identify those important subroutines.
+# Obfuscators usually apply arithmetic and virtualization-based obfuscations, which increases a lot the size of the target subroutine.
+# Aware of that, listing the instructions per subroutine can lead you to key subroutines in the binary.
 
 function_counts = [(func, len(list(func.low_level_il))) for func in bv.functions]
 function_counts.sort(key=lambda x: x[1], reverse=True)
