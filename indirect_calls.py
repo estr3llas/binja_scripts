1601552ccddcd42764805af5aef2cdfed03279b4
estr3llas <126114608+estr3llas@users.noreply.github.com>
2024-02-22 03:40:43 -0300

    Update indirect_calls.py

--- indirect_calls.py
+++ indirect_calls.py
@@ -2,8 +2,6 @@ target_reg = ['eax', 'ebx', 'ecx', 'edx', 'esi', 'edi', 'esp', 'ebp',
               'rax', 'rbx', 'rcx', 'rdx', 'rsi', 'rdi', 'rsp', 'rbp',
               'r8', 'r9', 'r10', 'r11', 'r12', 'r13', 'r14', 'r15']
 
-# Currently struggling to identify indirect calls like: "call dword [...", "call [rsp...]". Any help is welcome.
-
 # The following code iterates through each function to identify and display indirect calls, potentially generating a considerable amount of output.
 
 #"""
@@ -35,6 +33,24 @@ for func in bv.functions:
                     print(f'   {hex(instr.address)} {instr}')
     
                 if instr.operation == LowLevelILOperation.LLIL_JUMP and operand in target_reg:
+                    bv.add_tag(instr.address,"Important", "Indirect Call") 
+                    if current_function != func.name:
+                        if current_function:
+                            print()
+                        print(f'{func.name}:')
+                        current_function = func.name
+                    print(f'   {hex(instr.address)} {instr}')
+
+                if instr.operation == LowLevelILOperation.LLIL_CALL and operand.endswith("q"):
+                    bv.add_tag(instr.address,"Important", "Indirect Call") 
+                    if current_function != func.name:
+                        if current_function:
+                            print()
+                        print(f'{func.name}:')
+                        current_function = func.name
+                    print(f'   {hex(instr.address)} {instr}')
+
+                if instr.operation == LowLevelILOperation.LLIL_JUMP and operand.endswith("q"):
                     bv.add_tag(instr.address,"Important", "Indirect Call") 
                     if current_function != func.name:
                         if current_function:
@@ -82,4 +98,22 @@ for block in func.low_level_il:
                     print(f'{func.name}:')
                     current_function = func.name
                 print(f'   {hex(instr.address)} {instr}')
+
+                                if instr.operation == LowLevelILOperation.LLIL_CALL and operand.endswith("q"):
+                    bv.add_tag(instr.address,"Important", "Indirect Call") 
+                    if current_function != func.name:
+                        if current_function:
+                            print()
+                        print(f'{func.name}:')
+                        current_function = func.name
+                    print(f'   {hex(instr.address)} {instr}')
+
+                if instr.operation == LowLevelILOperation.LLIL_JUMP and operand.endswith("q"):
+                    bv.add_tag(instr.address,"Important", "Indirect Call") 
+                    if current_function != func.name:
+                        if current_function:
+                            print()
+                        print(f'{func.name}:')
+                        current_function = func.name
+                    print(f'   {hex(instr.address)} {instr}')
 """  
